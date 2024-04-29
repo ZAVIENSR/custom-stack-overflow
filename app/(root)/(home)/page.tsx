9abfdcfe56c2251b363c2452ada9ecc56deddc53
Erol Ege Saglam <erolegesaglam@gmail.com>
2024-04-29 16:11:54 +0300

    Navbar

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home Page you are about to visit!</div>;
+  return <div></div>;
 };
 
 export default Home;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,4 @@
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "../context/ThemeProvider";
 import { Inter, Space_Grotesk } from "next/font/google";
 import type { Metadata } from "next";
@@ -47,15 +41,6 @@ export default function RootLayout({
     >
       <html lang="en">
         <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
-          <header>
-            <SignedOut>
-              <SignInButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header>
-
           <ThemeProvider>
             <main>{children}</main>
           </ThemeProvider>
