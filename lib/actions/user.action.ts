47088ff3a4249487d0f2c8781f507fcf3407864b
Erol Ege Saglam <erolegesaglam@gmail.com>
2024-05-27 17:39:01 +0300

    Latest

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,6 +1,14 @@
 "use server";
 import { connectToDatabase } from "../mongoose";
 import User from "@/database/user.model";
+import {
+  CreateUserParams,
+  DeleteUserParams,
+  UpdateUserParams,
+} from "./shared.types";
+import { revalidatePath } from "next/cache";
+import Question from "@/database/question.model";
+
 export async function getUserById(params: any) {
   try {
     connectToDatabase();
@@ -13,5 +21,59 @@ export async function getUserById(params: any) {
   } catch (error) {
     console.log(error);
     throw new Error();
+  }
+}
+
+export async function createUser(userData: CreateUserParams) {
+  try {
+    connectToDatabase();
+
+    const newUser = await User.create(userData);
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function updateUser(params: UpdateUserParams) {
+  try {
+    connectToDatabase();
+
+    const { clerkId, updateData, path } = params;
+    await User.findOneAndUpdate({ clerkId }, updateData, { new: true });
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+export async function deleteUser(params: DeleteUserParams) {
+  try {
+    connectToDatabase();
+
+    const { clerkId } = params;
+    const user = await User.findOneAndDelete({ clerkId });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    // Delet euser from database
+    // and questisons, answers, comments etc.
+
+    const userQuestionIds = await Question.find({ author: user._id }).distinct(
+      "_id"
+    );
+
+    await Question.deleteMany({ author: user._id });
+
+    const deletedUser = await User.findByIdAndDelete(user._id);
+
+    return deletedUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
   }
 }
